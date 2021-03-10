cc4f5a2cf5404954a1e2df43813fb3872578bd1e
Zain Khalid <izain1122@gmail.com>
2021-03-11 00:41:21 +0500

    Added New Cropper

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -22,13 +22,26 @@ class _HomePageState extends State<HomePage> {
 
     //Cropping the image
 
-    File croppedFile = await ImageCropper.cropImage(
+File croppedFile = await ImageCropper.cropImage(
       sourcePath: image.path,
-      ratioX: 1.0,
-      ratioY: 1.0,
-      maxWidth: 512,
-      maxHeight: 512,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      androidUiSettings: AndroidUiSettings(
+          toolbarTitle: 'Cropper',
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false),
+      iosUiSettings: IOSUiSettings(
+        minimumAspectRatio: 1.0,
+      )
     );
+    
 
     //Compress the image
 
