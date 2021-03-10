efe52969ce75bfa30ab9ac9bbed9b53c24f51c79
ZdsAlpha <abzsoft999@gmail.com>
2021-03-10 23:50:01 +0500

    Fixing project name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_click_pick_crop_compress/main.dart';
+import 'package:URDULENS/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
